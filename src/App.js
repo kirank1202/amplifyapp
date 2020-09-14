8b4b4808a4f55a52ae792fc5965af540a5e131e6
Kiran Kanakadandila <KiranK1202@Kirans-MacBook-Air.local>
2020-09-14 13:10:56 -0500

    changes before image

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <H1> Hello from Kiran </H1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
